057cfe053a1dd92c1c34a0180815ceec9113d8bd
Smith Ringtho <sringtho@gmail.com>
2023-08-04 07:47:51 +0300

    Added logic for filtering featured products

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,14 @@
+const Product = require('../models/product')
 
 const getProducts = async (req, res) => {
-    res.status(200).json({success: true, products: "products"})
+    const {featured} = req.query
+    const queryObject = {}
+
+    if(featured) {
+        queryObject.featured = featured === 'true' ? true : false
+    }
+    const products = await Product.find(queryObject)
+    res.status(200).json({success: true, products, nbHits: products.length })
 }
 
 
