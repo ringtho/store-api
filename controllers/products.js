7f81c9df6d7bd54ce1e0f8cf0480eb5dfc928193
Smith Ringtho <sringtho@gmail.com>
2023-08-04 08:04:20 +0300

    Added ability to filter products using name -regex and company

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,20 @@
 const Product = require('../models/product')
 
 const getProducts = async (req, res) => {
-    const {featured} = req.query
+    const { featured, company, name } = req.query
     const queryObject = {}
 
     if(featured) {
         queryObject.featured = featured === 'true' ? true : false
     }
+    if(company) {
+        queryObject.company = company
+    }
+
+    if(name) {
+        queryObject.name = {$regex: name, $options: 'i'}
+    }
+    
     const products = await Product.find(queryObject)
     res.status(200).json({success: true, products, nbHits: products.length })
 }
